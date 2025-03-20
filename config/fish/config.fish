5f2e0abfff9e653e49ff3c42a9d2f0591bb49d32
krbreyn <krbreyn@gmail.com>
2025-03-20 12:07:04 -0400

    update fish

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,10 +1,9 @@
-set -U fish_user_paths $fish_user_paths $HOME/go/bin
-
 if status is-interactive
     # Commands to run in interactive sessions can go here
     alias fetch='fastfetch -l small -c examples/21'
     eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
-    set -x GOPATH $HOME/go
-    set -x PATH $PATH $GOPATH/bin
+    set -gx GOROOT /usr/local/go
+    set -gx GOPATH /home/cb/go
+    set -gx PATH $GOROOT/bin $GOPATH/bin $PATH
     fortune | cowsay #| lolcat -b -r
 end
